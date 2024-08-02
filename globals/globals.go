ed75b0e77f41ea30d4728b7620c2fe7a67a01ab1
Yappaholic <sav.boyar@gmail.com>
2024-08-02 11:44:45 +0300

    init: initial commit

---
+++ globals/globals.go
@@ -0,0 +1,3 @@
+package globals
+
+var Number = 0
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"htmx/api"
+	"htmx/globals"
+	"htmx/views"
+
+	"github.com/a-h/templ"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func homeHandler(c echo.Context) error {
+	return render(c, views.Index())
+}
+func countHandler(c echo.Context) error {
+	globals.Number++
+	return render(c, views.Count(globals.Number))
+}
+
+func apiHandler(c echo.Context) error {
+	return render(c, views.ApiResult(api.ApiCall()))
+}
+func render(c echo.Context, cmp templ.Component) error {
+	return cmp.Render(c.Request().Context(), c.Response())
+}
+func main() {
+	e := echo.New()
+	e.Static("/static", "static")
+	e.Use(middleware.Logger())
+	e.GET("/", homeHandler)
+	e.POST("/count", countHandler)
+	e.Logger.Fatal(e.Start(":6969"))
+}
